eea74fa2ae87897201712fb7d06079ff09d8910c
mark mersenski <MJMERSENSKI@QG.com>
2017-12-06 18:10:05 -0600

    created an OrderController and OrderManager view. the view is connected to the database and pulling in all the order information. The webconfig was modified to use the NMJFoodsEntities connection string.

---
+++ FinalProject/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+using System.Net;
+
+namespace FinalProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult OrderManager()
+        {
+            using (NMJFoodsEntities db = new NMJFoodsEntities())
+            {
+                return View(db.Orders.ToList());
+            }
+        }
+    }
+}
