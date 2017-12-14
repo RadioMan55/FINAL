3243b3e285c4e95f5f18d02cab70571488506578
mark mersenski <MJMERSENSKI@QG.com>
2017-12-13 18:24:57 -0600

    committing to mark2

--- FinalProject/Controllers/OrderController.cs
+++ FinalProject/Controllers/OrderController.cs
@@ -18,5 +18,41 @@ namespace FinalProject.Controllers
                 return View(db.Orders.OrderByDescending(o => o.OrderDate).ToList());
             }
         }
+        // GET: Product/FilterProducts
+        public JsonResult FilterOrders(int? IdFilter,int? EmployeeId)
+        {
+            using (NMJFoodsEntities db = new NMJFoodsEntities())
+            {
+                if (IdFilter == null)
+                {
+                    Response.StatusCode = 400;
+                    return Json(new { }, JsonRequestBehavior.AllowGet);
+                }
+                var Orders = db.Orders.ToList();
+                if (IdFilter != null)
+                {
+                    Orders = Orders.Where(o => o.OrderID == IdFilter).ToList();
+                }
+                else
+                {
+                    if (EmployeeId != null)
+                    {
+                        Orders = Orders.Where(o => o.EmployeeID == EmployeeId).ToList();
+                    }
+                }
+                var OrderDTOs = (from o in Orders
+                                 orderby o.OrderID
+                                 select new OrderDTOs
+                                 {
+                                     OrderID = o.OrderID,
+                                     OrderDate = o.OrderDate.ToString(),
+                                     RequiredDate = o.RequiredDate.ToString(),
+                                     ShippedDate = o.ShippedDate.ToString(),
+                                     CustomerID = o.CustomerID,
+                                     EmployeeID = o.EmployeeID
+                                 }).ToList();
+                return Json(OrderDTOs, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
---
+++ FinalProject/Models/OrderDTOs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class OrderDTOs
+    {
+        public int OrderID { get; set; }
+        public String OrderDate { get; set; }
+        public String RequiredDate { get; set; }
+        public String ShippedDate { get; set; }
+        public int? CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+
+    }
+}
